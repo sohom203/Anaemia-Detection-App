073f219a742fb630b5bc7a826ab5087a2fac0ad1
SOHOM GHOSAL <ghosalsohom2003@gmail.com>
2025-08-30 22:16:56 +0530

    Delete Anaemia_website/Conjunctiva_Anaemia directory

--- Anaemia_website/Conjunctiva_Anaemia/static/script.js
+++
@@ -1,9 +0,0 @@
-function previewImage(event) {
-    const reader = new FileReader();
-    reader.onload = function () {
-        const preview = document.getElementById('preview');
-        preview.src = reader.result;
-        preview.classList.remove("d-none");
-    };
-    reader.readAsDataURL(event.target.files[0]);
-}
